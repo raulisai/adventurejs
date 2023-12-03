1dc0dde17c20696ea2415d5bbf7a5cd0df9453c8
raul monroy <raulisai97@gmail.com>
2023-12-02 18:28:32 -0600

    reto1 ok

--- reto1.js
+++ reto1.js
@@ -23,3 +23,47 @@ const firstRepeatedId3 = findFirstRepeated(giftIds3)
 console.log(firstRepeatedId3) // 5
 ¡Ojo! Los elfos dicen que esto es una prueba técnica de Google.
 */
+
+//Solucion 1
+function findFirstRepeated(gifts) {
+  let saveGifts = [];
+
+  for (var i = 0; i <= gifts.length; i++) {
+    if (i === 0) {
+      saveGifts.push(gifts[i]);
+    } else {
+      console.log("save length -->" + saveGifts.length);
+      for (var j = 0; j < saveGifts.length; j++) {
+        console.log(saveGifts[j] + "==" + gifts[i]);
+        if (gifts[i] === saveGifts[j]) {
+          return gifts[i];
+        }
+      }
+      saveGifts.push(gifts[i]);
+    }
+  }
+  return -1;
+}
+
+const giftIds = [2, 1, 3, 5, 33, 22,6,8,9,0,33,4,5, 3, 2];
+//const firstRepeatedId = findFirstRepeated(giftIds);
+//console.log(firstRepeatedId); // 3
+
+
+
+//Solucion 2
+function findFirstRepeated2(gifts) {
+    let saveGifts =new Set();
+    for(gift in gifts){
+        if(saveGifts.has(gifts[gift])){return gifts[gift];}
+            saveGifts.add(gifts[gift]);
+    }
+    return -1;
+  }
+  
+  const giftIds2 = [2, 1, 3, 5, 33, 22,6,8,9,0,4,5, 3, 2];
+  const firstRepeatedId2 = findFirstRepeated2(giftIds2);
+  console.log(firstRepeatedId2); // 3
+  
+  
+
